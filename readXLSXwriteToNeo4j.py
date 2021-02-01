5e3049ec2e137817177afb84c14801ab2a869038
aradgonen <gonena.arad@gmail.com>
2021-02-01 09:45:35 +0200

    all function created

--- readXLSXwriteToNeo4j.py
+++ readXLSXwriteToNeo4j.py
@@ -1,17 +1,23 @@
+import json
+import requests
 from openpyxl import load_workbook
-from py2neo import Graph
-db = Graph("bolt://neo4j:shMador1@localhost:7687")
-def addnewrackstodb(db,data):
+
+wb = load_workbook("SampleDCMap.xlsx")
+data = wb.active
+simplidc_server_url = "http://localhost:8080/api/barzelim"
+headers = {'content-type': 'application/json'}
+
+def addnewrackstodb(data):
     data_rows = data.max_row
 
     for row in range(2, data_rows + 1):
         cur_rack = {}
         cur_node = {}
         cur_link = {}
-        cur_rack['rack_id'] = data.cell(row, 1).value
-        cur_rack['network'] = data.cell(row, 21).value
+        cur_rack['name'] = data.cell(row, 1).value
+        cur_rack['networkId'] = data.cell(row, 21).value
         cur_rack['size'] = 42
-        create_racknode(db,cur_rack)
+        create_racknode(cur_rack)
         if(data.cell(row,4).value == "Storage"):
 
             cur_node['arrayType'] = data.cell(row,5).value + " " + data.cell(row,7).value
@@ -21,57 +27,40 @@ def addnewrackstodb(db,data):
             cur_node['extramgmtIps'] = data.cell(row,9).value
             cur_node['clusterName'] = data.cell(row,5).value
             cur_node['osType'] = data.cell(row,19).value
-            cur_node['uNumber'] = data.cell(row,2).value
+            cur_node['unumber'] = data.cell(row,2).value
             cur_node['arrayProtocol'] = data.cell(row,4).value
             cur_node['name'] = data.cell(row,5).value
             cur_node['rackNumber'] = data.cell(row,1).value
 
-            create_storagenode(db,cur_node)
+            create_storagenode(cur_node)
+            put_in_rack(cur_node['serialNumber'],cur_node['rackNumber'])
 
+def create_racknode(rackdata):
+    save_to_db_response = requests.post(simplidc_server_url+"/racks", data=json.dumps(rackdata), headers=headers)
 
-def create_racknode(db, rackdata):
-    rack_id = rackdata['rack_id']
-    size = rackdata['size']
-    network = rackdata['network']
-    db.run('CREATE(n:RackNode {name:$rack_id, networkId:$network,size:$size})',rack_id=rack_id,size=size,network=network)
+def create_storagenode(storagedata):
+    save_to_db_response = requests.post(simplidc_server_url+"/devices/storage", data=json.dumps(storagedata), headers=headers)
 
-def create_storagenode(db,storagedata):
-    arrayType = storagedata['arrayType']
-    serialNumber = storagedata['serialNumber']
-    osVersion = storagedata['osVersion']
-    vendor = storagedata['vendor']
-    extramgmtIps = storagedata['extramgmtIps']
-    clusterName = storagedata['clusterName']
-    osType = storagedata['osType']
-    uNumber = storagedata['uNumber']
-    arrayProtocol = storagedata['arrayProtocol']
-    name = storagedata['name']
-    rackNumber = storagedata['rackNumber']
-    db.run('CREATE(n:StorageNode:XDeviceNode {arrayType:$arrayType,serialNumber:$serialNumber,osVersion:$osVersion,vendor:$vendor,extramgmtIps:$extramgmtIps,clusterName:$clusterName,osType:$osType,uNumber:$uNumber,arrayProtocol:$arrayProtocol,name:$name,rackNumber:$rackNumber})', arrayType=arrayType,serialNumber=serialNumber,osVersion=osVersion,vendor=vendor,extramgmtIps=extramgmtIps,
-    clusterName=clusterName,osType=osType,uNumber=uNumber,arrayProtocol=arrayProtocol,name=name,rackNumber=rackNumber)
-    put_in_rack(db,serialNumber,rackNumber)
-def create_networknode(db,netwrokdata):
-    return
+def create_networknode(netwrokdata):
+    save_to_db_response = requests.post(simplidc_server_url+"/devices/network", data=json.dumps(netwrokdata), headers=headers)
 
-def create_servernode(db,serverdata):
-    return
+def create_servernode(serverdata):
+    save_to_db_response = requests.post(simplidc_server_url+"/devices/server", data=json.dumps(serverdata), headers=headers)
 
-def create_xdevicenode(db,xdevicedata):
-    return
+def create_xdevicenode(xdevicedata):
+    save_to_db_response = requests.post(simplidc_server_url+"/devices/xdevice", data=json.dumps(xdevicedata), headers=headers)
 
-def create_linkrelation(db,linkdata):
-    return
+def put_in_rack(serialNumber,rackNumber):
+    insert_url = simplidc_server_url +"/racks/insert?rackNumber="+str(rackNumber)+"&serialNumber="+serialNumber
+    save_to_db_response = requests.post(insert_url,headers=headers)
 
-def put_in_rack(db,serialNumber,rackNumber):
-    db.run("MATCH (a:StorageNode),(b:RackNode) WHERE a.serialNumber = '$serialNumber' AND b.name = '$rackNumber' CREATE (a)-[r:IN]->(b) RETURN type(r)",serialNumber=serialNumber,rackNumber=rackNumber)
-wb = load_workbook("SampleDCMap.xlsx")
-data = wb.active
-# data_rows = data.max_row
-# data_cols = data.max_column
-# for col in range (1,data_cols):
-#     for row in range(1,data_rows+1):
-#         print(data.cell(row,col).value)
-addnewrackstodb(db,data)
+def connect_to_lan(fromSerial,toSerial):
+    connect_lan_url = simplidc_server_url + "/lan/connect?fromSerial=" + fromSerial + "&toSerial=" + toSerial
+    save_to_db_response = requests.post(connect_lan_url, headers=headers)
 
+def connect_to_san(serverSerial,storageName):
+    connect_san_url = simplidc_server_url + "/san/connect?serverSerial=" + serverSerial + "&storageName=" + storageName
+    save_to_db_response = requests.post(connect_san_url, headers=headers)
+addnewrackstodb(data)
 
 
